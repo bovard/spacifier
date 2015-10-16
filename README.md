ffa6424715b541be253f0cfa02bdd66f417c35f3
Bovard Tiberi <bovard.tiberi@gmail.com>
2015-10-16 16:20:31 -0700

    basic spacifier

---
+++ README.md
@@ -0,0 +1,2 @@
+## spacifier
+F o r m a k i n g t h i n g s l i k e t h i s .
---
+++ client/main.jsx
@@ -0,0 +1,46 @@
+/** @jsx React.DOM */
+var React = require('react');
+
+var Well = require('react-bootstrap').Well;
+var Input = require('react-bootstrap').Input;
+var Button = require('react-bootstrap').Button;
+
+
+var Spacifier = React.createClass({
+    getInitialState: function() {
+        return {
+            "output": "T y p e h e r e ."
+        };
+    },
+    spacify: function() {
+        var words = this.refs.input.getValue();
+        words = words.split(" ").join("");
+        words = words.split("").join(" ")
+        this.setState({
+            output: words
+        });
+    },
+    render: function() {
+        return (
+            <div className="content">
+                <Well>
+                    <Input type="textarea" label="Input" ref="input" placeholder="Type here." />
+                    <Button onPress={this.spacify} onClick={this.spacify} type="success">Spacify!</Button>
+                    <br />
+                    <br />
+                    <p>Output:</p>
+                    <p>Length: {this.state.output.length}</p>
+                    <Well>
+                        <p>{this.state.output}</p>
+                    </Well>
+                </Well>
+            </div>
+        );
+    }
+});
+
+React.renderComponent(
+    <Spacifier />,
+    document.body
+);
+
